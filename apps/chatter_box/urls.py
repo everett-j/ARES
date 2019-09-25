eae6a71c7775a00e238c10141c25c634539476ac
Josh <jseverett16@gmail.com>
2019-09-25 07:26:52 -0700

    0726092519

--- apps/chatter_box/urls.py
+++ apps/chatter_box/urls.py
@@ -3,6 +3,6 @@ from . import views
                     
 urlpatterns = [
     url(r'^$', views.index),
-
-
+    url(r'^chatter$', views.chatter),
+    url(r'^send/(?P<text>\w+)$', views.send_text),
 ]
--- apps/chatter_box/views.py
+++ apps/chatter_box/views.py
@@ -1,8 +1,33 @@
 # Create your views here.
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
+from django.http import JsonResponse
+from json_response import JsonResponse
+
+
 
     
 def index(request):
     return render(request, 'chatter_box/index.html')
 
+
+def chatter(request):
+    chatbot = ChatBot('Ares', trainer='chatterbot.trainers.ListTrainer')
+    response = chatbot.get_response("Bot info.")
+    print(response)
+    response = chatbot.get_response("text")
+    print(response)
+
+    
+    return render(request, 'chatter_box/index.html')
+
+
+def send_text( request, text):
+    chatbot = ChatBot('Ares', trainer='chatterbot.trainers.ListTrainer')
+    # response = chatbot.get_response("Bot info.")
+    # print(response)
+    response = chatbot.get_response(text = text)
+    print(response)
+    return redirect("/")
